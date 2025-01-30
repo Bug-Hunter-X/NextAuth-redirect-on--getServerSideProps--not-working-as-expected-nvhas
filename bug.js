df9ce4963c5aeca979e678d89c36b4b82583e989
BugHunter-X <askzenai@gmail.com>
2025-01-30 14:36:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,23 @@
+```javascript
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+
+export default async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(context, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
+```
